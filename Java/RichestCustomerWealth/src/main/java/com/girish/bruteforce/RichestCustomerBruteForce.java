b8061c83408cae8f435dcc5e9afc1d3d5c6bb41a
GirishSrinivas <girish.s.srinivas@gmail.com>
2020-12-08 21:00:30 -0600

    code cleanup

--- Java/RichestCustomerWealth/src/main/java/com/girish/bruteforce/RichestCustomerBruteForce.java
+++ Java/RichestCustomerWealth/src/main/java/com/girish/bruteforce/RichestCustomerBruteForce.java
@@ -3,13 +3,11 @@ package com.girish.bruteforce;
 public class RichestCustomerBruteForce {
     public int maximumWealth(int[][] accounts) {
         int maxWealth = 0;
-        int m;
-        int n;
 
         if(accounts != null) {
             if (accounts.length > 0 && accounts[0] != null) {
-                m = accounts.length;
-                n = accounts[0].length;
+                int m = accounts.length;
+                int n = accounts[0].length;
                 if (m <= 50 && (n > 0 && n <= 50)) {
                     System.out.println("Logic in action...");
                     maxWealth = 1;
